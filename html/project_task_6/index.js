b637be6a8e38a59c0a7a825d6508e5909989934d
anuragrao42 <anurag.18003@iitgoa.ac.in>
2020-09-22 12:44:22 +0530

    task_6 kelvin weather completed

---
+++ html/project_task_6/index.js
@@ -0,0 +1,17 @@
+//today's forecast
+const kelvin = 293;
+//Kelvin temp converted to Celsius
+const celsius = kelvin - 273;
+//Celsius to fahrenheit
+var fahrenheit = (9 / 5) * celsius + 32;
+//rounding off fahrenheit using floor function
+fahrenheit = Math.floor(fahrenheit);
+
+console.log("The temperature is " + fahrenheit + " degrees Fahrenheit");
+
+//Ceslius to Newton
+var newton = (33 / 100) * celsius;
+newton = Math.floor(newton);
+console.log("The temperature is " + newton + " degrees Newton.");
+
+// 0 deg. Kelvin can be represented as -460 deg. Fahrenheit
